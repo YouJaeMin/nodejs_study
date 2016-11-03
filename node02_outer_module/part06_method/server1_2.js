6251b6a8e593828d6fb74e0de55e7e17aca7741d
YouJaeMin <wovkek@naver.com>
2016-11-03 14:49:57 +0900

    method

--- node02_outer_module/part06_method/server1_2.js
+++ node02_outer_module/part06_method/server1_2.js
@@ -4,6 +4,7 @@
 
 var http = require("http");
 var fs = require("fs");
+var querystring = require("querystring");
 
 var server = http.createServer(function(req, resp) {
 
@@ -17,17 +18,24 @@ var server = http.createServer(function(req, resp) {
 
 	} else if (req.method == "POST") {
 		var res = "";
+		
 		req.on("data", function(data) {
 			// data 이벤트가 발생이 되면 res변수에 parameter값을 추가한다.
-			res = data;
+			res += data;
 		});
 		
 		req.on("end", function() {
-			console.log(typeof(res));
-			resp.writeHead(200, {
-				"Content-Type" : "text/html;charset=UTF-8"
-			});
-			resp.end(res);
+			// res는 문자열
+			// console.log(typeof (res));
+			
+			//JSON 객체로 변환
+			//{ data_a: 'dsfsf', data_b: 'sdfs' }
+			var parsed = querystring.parse(res);
+			var aParam = parsed.data_a;
+			var bParam = parsed.data_b;
+			
+			resp.writeHead(200, {"Content-Type" : "text/html;charset=UTF-8"});
+			resp.end("<div><p>"+ aParam +"</p><p>"+ bParam +"</p></div>");
 		});
 		
 	}
